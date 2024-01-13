a72af09e358b95c6c5c1a45fc80d7ede40623df5
CoolElectronics <me@coolelectronics.me>
2024-01-12 19:41:24 -0500

    fix several innaccuracies

--- src/main.ts
+++ src/main.ts
@@ -1,72 +1,6 @@
 import { setBareClientImplementation, Client, BareHeaders, BareResponse, GetRequestHeadersCallback, MetaCallback, ReadyStateCallback, WebSocketImpl } from "@mercuryworkshop/bare-client-custom";
 // import { libcurl } from "../libcurl.js/client/out/libcurl.js";
 
-const status_messages = {
-  100: "Continue",
-  101: "Switching Protocols",
-  102: "Processing",
-  103: "Early Hints",
-  200: "OK",
-  201: "Created",
-  202: "Accepted",
-  203: "Non-Authoritative Information",
-  204: "No Content",
-  205: "Reset Content",
-  206: "Partial Content",
-  207: "Multi-Status",
-  208: "Already Reported",
-  226: "IM Used",
-  300: "Multiple Choices",
-  301: "Moved Permanently",
-  302: "Found",
-  303: "See Other",
-  304: "Not Modified",
-  305: "Use Proxy",
-  306: "Switch Proxy",
-  307: "Temporary Redirect",
-  308: "Permanent Redirect",
-  400: "Bad Request",
-  401: "Unauthorized",
-  402: "Payment Required",
-  403: "Forbidden",
-  404: "Not Found",
-  405: "Method Not Allowed",
-  406: "Not Acceptable",
-  407: "Proxy Authentication Required",
-  408: "Request Timeout",
-  409: "Conflict",
-  410: "Gone",
-  411: "Length Required",
-  412: "Precondition Failed",
-  413: "Payload Too Large",
-  414: "URI Too Long",
-  415: "Unsupported Media Type",
-  416: "Range Not Satisfiable",
-  417: "Expectation Failed",
-  418: "I'm a teapot",
-  421: "Misdirected Request",
-  422: "Unprocessable Content",
-  423: "Locked",
-  424: "Failed Dependency",
-  425: "Too Early",
-  426: "Upgrade Required",
-  428: "Precondition Required",
-  429: "Too Many Requests",
-  431: "Request Header Fields Too Large",
-  451: "Unavailable For Legal Reasons",
-  500: "Internal Server Error",
-  501: "Not Implemented",
-  502: "Bad Gateway",
-  503: "Service Unavailable",
-  504: "Gateway Timeout",
-  505: "HTTP Version Not Supported",
-  506: "Variant Also Negotiates",
-  507: "Insufficient Storage",
-  508: "Loop Detected",
-  510: "Not Extended",
-  511: "Network Authentication Required"
-}
-
 export class TLSClient extends Client {
   queue: (() => void)[] = [];
   canstart = true;
@@ -85,7 +19,7 @@ export class TLSClient extends Client {
       this.mux = mux;
       declare var wasm_bindgen: any;
       wasm_bindgen("./wstcp_client_bg.wasm").then(async () => {
-        let tcp = await new wasm_bindgen.WsTcpWorker(mux, navigator.userAgent);
+        let tcp = await new wasm_bindgen.WsTcp(mux, navigator.userAgent, 10);
         window.tcp = tcp;
         this.tcp = tcp;
       });
@@ -127,7 +61,6 @@ export class TLSClient extends Client {
               headers,
             }) as BareResponse);
             // setTimeout(() => {
-            console.log("poppin!");
             this.canstart = true;
             this.queue.pop()!();
             // }, 1000);
@@ -144,7 +77,6 @@ export class TLSClient extends Client {
 
         setTimeout(() => {
 
-          // the vk6headers are weird
           const headers = new Headers();
           for (const [header, value] of Object.entries(payload.headers)) {
             headers.append(header, value);
@@ -158,18 +90,16 @@ export class TLSClient extends Client {
         });
       });
     } else {
-      let payload = await this.tcp.fetch(remote.href, { method, body, requestHeaders });
+      console.log(remote.href, { method, body, headers: requestHeaders });
+      let payload = await this.tcp.fetch(remote.href, { method, body, headers: requestHeaders });
+      console.log(payload);
+      return payload as BareResponse;
 
-      const headers = new Headers();
-      for (const [header, value] of payload.headers) {
-        headers.append(header, value);
-      }
-
-      return new Response(payload.body, {
-        status: payload.status,
-        statusText: status_messages[payload.status],
-        headers,
-      }) as BareResponse;
+      // return new Response(payload.body, {
+      //   status: payload.status,
+      //   statusText: status_messages[payload.status],
+      //   headers,
+      // }) as BareResponse;
     }
   }
 
