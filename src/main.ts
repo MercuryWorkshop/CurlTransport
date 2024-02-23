bc4e039850e9ae17bc051265e5e00e9791e8b8f6
CoolElectronics <me@coolelectronics.me>
2024-02-23 17:33:56 -0500

    bcc2

--- src/main.ts
+++ src/main.ts
@@ -1,58 +1,52 @@
-import { setBareClientImplementation, Client, BareHeaders, BareResponse, GetRequestHeadersCallback, MetaCallback, ReadyStateCallback, WebSocketImpl } from "@mercuryworkshop/bare-client-custom";
+import { BareHeaders, BareResponse, TransferrableResponse, type BareTransport } from "@mercuryworkshop/bare-mux";
 import { libcurl } from "libcurl.js";
 //@ts-ignore
 import epoxy from "@mercuryworkshop/epoxy-tls";
 
-export class TLSClient extends Client {
-  queue: (() => void)[] = [];
+export class TLSClient implements BareTransport {
   canstart = true;
-
-  wsproxy;
   epxclient: Awaited<ReturnType<typeof epoxy>>["EpoxyClient"]["prototype"];
+  wsproxy: string | null;
 
   constructor({ wsproxy, mux }) {
-    super();
     if (wsproxy) {
       libcurl.load_wasm('libcurl.wasm')
       libcurl.set_websocket(wsproxy);
       //@ts-ignore
       this.wsproxy = wsproxy
-  } else if (mux) {
-    console.log("load start");
-
-    (async () => {
-      let { EpoxyClient } = await epoxy();
-      this.epxclient = await new EpoxyClient(mux, navigator.userAgent, 10);
-    })();
-  }
+    } else if (mux) {
+      (async () => {
+        let { EpoxyClient } = await epoxy();
+        this.epxclient = await new EpoxyClient(mux, navigator.userAgent, 10);
+      })();
+    }
   }
 
   async request(
-    method: string,
-    requestHeaders: BareHeaders,
-    body: BodyInit | null,
     remote: URL,
-    cache: string | undefined,
-    duplex: string | undefined,
-    signal: AbortSignal | undefined,
-    arrayBufferImpl: ArrayBufferConstructor
-  ): Promise<BareResponse> {
+    method: string,
+    body: BodyInit | null,
+    headers: BareHeaders,
+    signal: AbortSignal | undefined
+  ): Promise<TransferrableResponse> {
     if (this.wsproxy) {
       return libcurl.fetch(remote.href, {
-          method,
-          headers: requestHeaders,
-          body,
-          redirect: "manual",
-        })
+        method,
+        headers: headers,
+        body,
+        redirect: "manual",
+      })
     } else {
       if (body instanceof Blob)
         body = await body.arrayBuffer();
-      try {
-        let payload = await this.epxclient.fetch(remote.href, { method, body, headers: requestHeaders, redirect: "manual" });
-        return payload as BareResponse;
-      } catch (e) {
-        return new Response() as BareResponse;
-      }
+      let payload = await this.epxclient.fetch(remote.href, { method, body, headers, redirect: "manual" });
+
+      return {
+        body: payload.body!,
+        headers: (payload as any).rawHeaders,
+        status: payload.status,
+        statusText: payload.statusText,
+      };
     }
   }
 
@@ -85,57 +79,57 @@ export class TLSClient extends Client {
         e.stopImmediatePropagation();
         initialErrorHappened = true;
       }
-    });  
+    });
     type WebSocketData = string | ArrayBuffer | Blob;
-    
+
     // coerce iframe Array type to our window array type
     protocols = Array.from(protocols);
-    let origin = arrayBufferImpl.prototype.constructor.constructor("return __uv$location")().origin;  
-    if (this.wsproxy) {  
-        let wsws = new libcurl.WebSocket(remote.toString(), protocols);
-        
-        wsws.onopen = (protocol: string) => {
-          onReadyState(WebSocket.OPEN);
-          (ws as any).__defineGetter__("protocol", () => { return protocol });
-          Object.defineProperty(wsws, 'binaryType', {
-            get: function () {
-              return ws.binaryType;
-            },
-          });
-          console.log(wsws)
-          ws.dispatchEvent(new Event("open"));
-        }
-        //@ts-ignore
-        wsws.onclose = (code: number, reason: string, wasClean: boolean) => {
-          onReadyState(WebSocket.CLOSED);
-          ws.dispatchEvent(new CloseEvent("close", { code, reason, wasClean }));
-        }
-         //@ts-ignore
-        wsws.onerror = (message: string) => {
-          ws.dispatchEvent(new ErrorEvent("error", { message }));
-        }
-         //@ts-ignore
-        wsws.onmessage = (event) => {
-          let payload = event.data
-          if (typeof payload === "string") {
-            ws.dispatchEvent(new MessageEvent("message", { data: payload }));
-          } else if (payload instanceof ArrayBuffer) {
-            Object.setPrototypeOf(payload, arrayBufferImpl.prototype);
+    let origin = arrayBufferImpl.prototype.constructor.constructor("return __uv$location")().origin;
+    if (this.wsproxy) {
+      let wsws = new libcurl.WebSocket(remote.toString(), protocols);
 
-            ws.dispatchEvent(new MessageEvent("message", { data: payload }));
-          } else if (payload instanceof Blob) {
-            console.log(payload)
-            console.log(event)
-            ws.dispatchEvent(new MessageEvent("message", { data: payload }));
-          }
+      wsws.onopen = (protocol: string) => {
+        onReadyState(WebSocket.OPEN);
+        (ws as any).__defineGetter__("protocol", () => { return protocol });
+        Object.defineProperty(wsws, 'binaryType', {
+          get: function() {
+            return ws.binaryType;
+          },
+        });
+        console.log(wsws)
+        ws.dispatchEvent(new Event("open"));
+      }
+      //@ts-ignore
+      wsws.onclose = (code: number, reason: string, wasClean: boolean) => {
+        onReadyState(WebSocket.CLOSED);
+        ws.dispatchEvent(new CloseEvent("close", { code, reason, wasClean }));
+      }
+      //@ts-ignore
+      wsws.onerror = (message: string) => {
+        ws.dispatchEvent(new ErrorEvent("error", { message }));
+      }
+      //@ts-ignore
+      wsws.onmessage = (event) => {
+        let payload = event.data
+        if (typeof payload === "string") {
+          ws.dispatchEvent(new MessageEvent("message", { data: payload }));
+        } else if (payload instanceof ArrayBuffer) {
+          Object.setPrototypeOf(payload, arrayBufferImpl.prototype);
+
+          ws.dispatchEvent(new MessageEvent("message", { data: payload }));
+        } else if (payload instanceof Blob) {
+          console.log(payload)
+          console.log(event)
+          ws.dispatchEvent(new MessageEvent("message", { data: payload }));
         }
-        ws.send = (data: any) => {
-          wsws.send(data);
-        };
-        ws.close = () => {
-          wsws.close();
-        }
-        return ws;
+      }
+      ws.send = (data: any) => {
+        wsws.send(data);
+      };
+      ws.close = () => {
+        wsws.close();
+      }
+      return ws;
     } else {
       let wsws = this.epxclient.connect_ws(
         (protocol: string) => {
